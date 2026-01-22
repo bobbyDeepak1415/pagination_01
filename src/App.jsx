c494b92ed475e7abdd81d541b899cfe4207f6822
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-23 00:20:45 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-
-const Page_Size=10
+const Page_Size = 10;
 
 function App() {
   const [allComments, setAllComments] = useState([]);
@@ -22,32 +21,40 @@ function App() {
     fetchData();
   }, []);
 
-const startIndex=page*Page_Size
+  const startIndex = page * Page_Size;
 
-const currentComments=allComments.slice(startIndex,startIndex+Page_Size)
+  const currentComments = allComments.slice(startIndex, startIndex + Page_Size);
 
   const handleClickBack = () => {
-    if(page>0){
-      setPage(prev=>prev-1)
+    if (page > 0) {
+      setPage((prev) => prev - 1);
     }
   };
 
   const handleClickForward = () => {
-    if(startIndex+Page_Size<allComments.length){
-      setPage(prev=>prev+1)
+    if (startIndex + Page_Size < allComments.length) {
+      setPage((prev) => prev + 1);
     }
   };
 
   return (
     <div>
-      <ol>
+      <ol start={page * Page_Size + 1}>
         {currentComments.map((comment) => {
           return <li key={comment.id}>{comment.name}</li>;
         })}
       </ol>
 
-      <button disabled={page===0} onClick={handleClickBack}>back</button>
-      <button disabled={startIndex+Page_Size>=allComments.length} onClick={handleClickForward}>next</button>
+      <button disabled={page === 0} onClick={handleClickBack}>
+        back
+      </button>
+      <button
+        disabled={startIndex + Page_Size >= allComments.length}
+        onClick={handleClickForward}
+      >
+        next
+      </button>
+      <p>{page}</p>
     </div>
   );
 }
