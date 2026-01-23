10c3679e86ab78e5172a2de7ec806f140e1e57c9
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-23 17:45:02 +0530

    boiler plate done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,63 +1,30 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
-const Page_Size = 10;
+const Demo = () => {
 
-function Demo() {
-  const [allComments, setAllComments] = useState([]);
-  const [page, setPage] = useState(0);
+    useEffect(()=>{
+const fetchData=async()=>{
+    try{
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/comments",
-        );
-        setAllComments(response.data);
-      } catch (e) {
-        console.log("failed to fetch..", e);
-      }
-    };
-
-    fetchData();
-  });
-
-  const handlePrevClick = () => {
-    if (page > 0) {
-      setPage((prev) => prev - 1);
+        const response=await axios.get("https://jsonplaceholder.typicode.com/comments")
+        set
+    }catch(e){
+console.log("failed to fetch...",e)
     }
-  };
 
-  const handleNextClick = () => {
-    if (startIndex + Page_Size <= allComments.length) {
-      setPage((prev) => prev + 1);
-    }
-  };
-
-  const startIndex = page * Page_Size;
-
-  const currentComments = allComments.slice(startIndex, startIndex + Page_Size);
+}
+fetchData()
+    },[])
 
   return (
     <div>
-      {/* <h1>Hello</h1> */}
-      <ol>
-        {currentComments.map((comment) => {
-          return <li key={comment.id}>{comment.name}</li>;
-        })}
-      </ol>
-
-      <button disabled={page === 0} onClick={handlePrevClick}>
-        Previous
-      </button>
-      <button
-        disabled={startIndex + Page_Size >= allComments.length}
-        onClick={handleNextClick}
-      >
-        Next
-      </button>
+      Hello
     </div>
-  );
+  )
 }
 
-export default Demo;
+export default Demo
+
+
+//   "",
