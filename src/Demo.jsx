6dc1d5650039b02805642e6e3cb4bd204dc054f1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-23 17:24:26 +0530

    boiler plate done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+
+const Page_Size=10
 
 function Demo() {
+  const [allComments, setAllComments] = useState([]);
+  const [page,setPage]=useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments",
+        );
+        setAllComments(response.data);
+      } catch (e) {
+        console.log("failed to fetch..", e);
+      }
+    };
+
+    fetchData();
+  });
+
+  const startIndex=page*Page_Size
+
+  const currentComments=allComments.slice(startIndex,startIndex+Page_Size)
+
+
   return (
     <div>
-      
+      {/* <h1>Hello</h1> */}
+      <ol>
+        {currentComments.map((comment) => {
+          return <li key={comment.id}>{comment.name}</li>;
+        })}
+      </ol>
+
+      <button>Previous</button>
+      <button>Next</button>
     </div>
-  )
+  );
 }
 
-export default Demo
+export default Demo;
