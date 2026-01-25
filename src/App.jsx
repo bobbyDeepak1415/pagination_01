02b55a92727920fdd9194db02c44ee20500b98d7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 16:58:36 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./App.css";
+// import "./App.css";
 import axios from "axios";
 
 const Page_Size = 10;
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+const Page_Size = 10;
 const Demo = () => {
+  const [allComments, setAllComments] = useState([]);
+
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments",
+        );
+
+        setAllComments(response.data);
+      } catch (e) {
+        console.log("failed to fetch...", e);
+      }
+    };
+
+    fetchData();
+  });
+
+  const startIndex = Page_Size * page;
+
+  const currentComments = allComments.slice(startIndex, startIndex + Page_Size);
+
+  const handleClickBack=()=>{
+    
+  }
+
   return (
     <div>
-      
+      <ul>
+        {currentComments.map((comment) => {
+          return <li key={comment.id}>{comment.name}</li>;
+        })}
+      </ul>
+      <div>
+        <button onClick={handleClickBack} disabled={page === 0}>
+          Prev
+        </button>
+        <button
+          onClick={handleClickNext}
+          disabled={startIndex + Page_Size >= allComments.length}
+        >
+          Next
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
