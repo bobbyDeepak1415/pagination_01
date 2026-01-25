2676dc6e28ac77a788df50c9dbc353ed5c9b7f31
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-25 18:14:02 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const PageSize = 10;
 
 const Demo = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  )
-}
+  const [comments, setComments] = useState([]);
 
-export default Demo
+  const [page,setPage]=useState(0)
 
-"https://jsonplaceholder.typicode.com/comments",
+
+
+  useEffect(() => {
+    const fetchData = () => {
+      try {
+        const response = axios.get(
+          "https://jsonplaceholder.typicode.com/comments",
+        );
+        setComments(response.data);
+      } catch (e) {
+        console.log("failed to fecth", e);
+      }
+    };
+
+
+
+    fetchData();
+  });
+
+const startIndex=page*PageSize
+  const currentComments=comments.splice(startIndex,startIndex+PageSize)
+
+
+  return <div className="h-[100vh] bg-slate-400"></div>;
+};
+
+export default Demo;
