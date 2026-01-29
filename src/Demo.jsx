b3ff8591cc78606bbca02fb2b0202256b58b418b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-30 02:27:35 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,66 +1,7 @@
-import axios from "axios";
-// import "./App.css";
+import React from "react";
 
-import React, { useEffect, useState } from "react";
-
-const Page_Size = 10;
-
-function Demo() {
-  const [comments, setComments] = useState([]);
-
-  const [page, setPage] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/comments",
-        );
-        setComments(response.data);
-      } catch (e) {
-        console.log("failed To fetch", e);
-      }
-    };
-
-    fetchData();
-  });
-
-  const handleClickPrev = () => {
-    if (page > 0) setPage((prev) => prev - 1);
-  };
-
-  const handleClickNext = () => {
-    if (startIndex + Page_Size >= comments.length) return;
-
-    setPage((prev) => prev + 1);
-  };
-
-  const startIndex = page * Page_Size;
-
-  const currentComments = comments.slice(startIndex, startIndex + Page_Size);
-
-  return (
-    <div style={{ height: "100vh", backgroundColor: "slategray" }}>
-      <div>
-        <ol start={page * Page_Size + 1}>
-          {currentComments.map((comment) => {
-            return <li>{comment.name}</li>;
-          })}
-        </ol>
-      </div>
-      <div>
-        <button disabled={page === 0} onClick={() => handleClickPrev()}>
-          Prev
-        </button>
-        <button
-          disabled={startIndex + Page_Size >= comments.length}
-          onClick={() => handleClickNext()}
-        >
-          Next
-        </button>
-      </div>
-    </div>
-  );
-}
+const Demo = () => {
+  return <div></div>;
+};
 
 export default Demo;
