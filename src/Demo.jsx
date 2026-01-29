1e4737b7519ca91fecbe55a3cd811890dd5aa997
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-30 02:21:15 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,10 +1,49 @@
-import React from "react";
+import axios from "axios";
+// import "./App.css";
+
+import React, { useEffect, useState } from "react";
+
+const Page_Size = 10;
 
 function Demo() {
-  return <div>Hello</div>;
+  const [comments, setComments] = useState([]);
+
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments",
+        );
+        setComments(response.data);
+      } catch (e) {
+        console.log("failed To fetch", e);
+      }
+    };
+
+    fetchData();
+  });
+
+  const startIndex = page * Page_Size;
+
+  const currentComments = comments.slice(startIndex, startIndex + Page_Size);
+
+  return (
+    <div style={{ height: "100vh", backgroundColor: "slategray" }}>
+      <div>
+        <ul>
+          {currentComments.map((comment) => {
+            return <li>{comment.name}</li>;
+          })}
+        </ul>
+      </div>
+      <div>
+        <button>Prev</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 }
 
 export default Demo;
-
-
-        // "https://jsonplaceholder.typicode.com/comments",
