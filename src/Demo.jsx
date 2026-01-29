7e670116693941c09074fea52203dd0c7e809628
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-01-30 02:11:12 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,37 +1,10 @@
-import axios from "axios";
-// import { comment } from "postcss";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-const PageSize = 10;
-
-const Demo = () => {
-  const [allComments, setAllComments] = useState([]);
-
-  useEffect(() => {
-    const fetchData = () => {
-      try {
-        const response = axios.get(
-          "https://jsonplaceholder.typicode.com/comments",
-        );
-        setAllComments(response.data);
-      } catch (e) {
-        console.log("failed to fecth", e);
-      }
-    };
-
-    fetchData();
-  });
-
-  return (
-    <div>
-       <ol>
-        {allComments.map((comment) => {
-          return <li key={comment.id}>{comment.name}</li>;
-        })}
-      </ol> 
-      
-    </div>
-  );
-};
+function Demo() {
+  return <div>Hello</div>;
+}
 
 export default Demo;
+
+
+        // "https://jsonplaceholder.typicode.com/comments",
