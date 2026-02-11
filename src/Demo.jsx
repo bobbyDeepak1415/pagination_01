5374bd6e17dee4d19bfc0ea82628a69db6526b28
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-02-11 17:44:37 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 
 const Demo = () => {
+  const [count, setCount] = useState(0);
 
-  const [count,setCount]=useState(0)
-
-  const handleClick=()=>{
-    for(let i=0;i<5;i++){
-      setCount(count+1)
+  const handleClick = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount(prev=>prev + 1);
     }
-  }
+  };
 
-  return <div>
-
-    <button onClick={handleClick}>{count}</button>
-  </div>;
+  return (
+    <div>
+      <button onClick={handleClick}>{count}</button>
+    </div>
+  );
 };
 
 export default Demo;
