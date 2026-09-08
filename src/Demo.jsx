dd8727b61e00cad15b941bcb2f0fc5ef9005de3e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-09 01:54:42 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,48 +1,9 @@
-import React, { useState } from 'react'
-import useFetchData from './useFetchData'
-
-
-const url="https://jsonplaceholder.typicode.com/comments"
-
-let PAGE_SIZE=10
-
-
+import React from 'react'
 
 const Demo = () => {
-
-
-        const {allComments}=useFetchData(url)
-        const [page,setPage]=useState(0)
-
-        let startIndex=page*PAGE_SIZE
-
-       let currentComments=allComments.slice(startIndex,startIndex+PAGE_SIZE)
-
-const handlePrevClick=()=>{
-  if(page===0) return
-  setPage(prev=>prev-1)
-
-}
-const handleNextClick=()=>{
-  if(startIndex+PAGE_SIZE<allComments.length){
-    setPage(prev=>prev+1)
-  }
-
-}
-
-
   return (
-    <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
-      <h2>Comment List:</h2>
-      <ol start={page*PAGE_SIZE+1}>
-
-      {currentComments.map((comment)=>{
-        return <li key={comment.id}>{comment.name}</li>
-      })}
-      </ol>
-      <button disabled={page===0} onClick={handlePrevClick}>Prev</button>
-      <button disabled={startIndex+PAGE_SIZE>=allComments.length} onClick={handleNextClick}>Prev</button>
-      <p>Page:{page+1}</p>
+    <div>
+      hello
     </div>
   )
 }
