bf41311f92bc3fd781609518af61f9c501705bdd
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-09 01:47:49 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useFetchData from './useFetchData'
+
+
+const url="https://jsonplaceholder.typicode.com/comments"
+
+let PAGE_SIZE=10
+
+
 
 const Demo = () => {
 
-        let url="https://jsonplaceholder.typicode.com/comments"
 
         const {allComments}=useFetchData(url)
+        const [page,setPage]=useState(0)
+
+        let startIndex=page*PAGE_SIZE
+
+       let currentComments=allComments.slice(startIndex,startIndex+PAGE_SIZE)
+
+const handlePrevClick=()=>{
+  
+}
+
 
   return (
-    <div>
-      Hello
+    <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
+      <h2>Comment List:</h2>
+      <ol>
+
+      {currentComments.map((comment)=>{
+        return <li key={comment.id}>{comment.name}</li>
+      })}
+      </ol>
+      <button onClick={handlePrevClick}>Prev</button>
+      <p>Page:{page}</p>
     </div>
   )
 }
