05eef59cb239ac9645b5969b07f842c63f0e682b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-09 15:02:10 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
+
+let PAGE_SIZE=10
+
+const url="https://jsonplaceholder.typicode.com/comments"
 const Demo = () => {
+
+
+  const [allComments,setAllComments]=useState([])
+  const [page,setPage]=useState(0)
+
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const res=await axios.get(url)
+      
+setAllComments(res.data)
+
+    }
+    fetchData()
+  },[])
+
+
+  let startIndex=page*PAGE_SIZE
+
+  const currentComments=allComments.slice(startIndex,startIndex+PAGE_SIZE)
+
   return (
     <div>
-      hello
+      <h2>Comments List:</h2>
+      <ol>
+
+      {currentComments.map((comment)=>{
+        return <li key={comment.id}>{comment.name}</li>
+      })}
+      </ol>
+      
     </div>
   )
 }
