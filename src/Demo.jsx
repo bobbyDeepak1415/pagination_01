bfd7ee064f117a49298439f66b13dd354781c8b0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-09 15:51:21 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -2,12 +2,30 @@ import React, { useState } from 'react'
 
 const Demo = () => {
 
+  
   const [item,setItem]=useState("")
+const [itemList,setItemList]=useState([])
+
+
+const handleAddItem=()=>{
+  if(!item.trim()) return
+  setItemList([...itemList,item])
+  setItem("")
+}
+
   return (
     <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
 
       <input value={item} onChange={(e)=>setItem(e.target.value)}/>
-      
+      <button onClick={handleAddItem}>Add Item</button>
+      <div>
+        <ul>
+
+        {itemList.map((item,index)=>{
+          return <li key={index}>{item}</li>
+        })}
+        </ul>
+      </div>
     </div>
   )
 }
