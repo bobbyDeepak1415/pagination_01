d421169586734ea02f6911a0cd02a0b22f2d98cc
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-09 15:38:37 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,63 +1,22 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
-
-let PAGE_SIZE=10
-
-const url="https://jsonplaceholder.typicode.com/comments"
 const Demo = () => {
 
+  const inputFileRef=useRef(0)
 
-  const [allComments,setAllComments]=useState([])
-  const [page,setPage]=useState(0)
-
-
-  useEffect(()=>{
-    const fetchData=async()=>{
-      const res=await axios.get(url)
-      
-setAllComments(res.data)
-
-    }
-    fetchData()
-  },[])
-
-
-  let startIndex=page*PAGE_SIZE
-
-  const currentComments=allComments.slice(startIndex,startIndex+PAGE_SIZE)
-
-
-  const handlePrevClick=()=>{
-    if(page===0) return
-
-    setPage(prev=>prev-1)
-
-  }
-
-  const handleNextClick=()=>{
-    if(startIndex+PAGE_SIZE>=allComments.length) return
-    setPage(prev=>prev+1)
-
-
+  const handleClick=()=>{
+    inputFileRef.current.click()
+    inputFileRef.current.value=""
 
   }
 
   return (
-    <div style={{height:"100vh",width:"100vw",backgroundColor:"gray"}}>
-      <h2>Comments List:</h2>
-      <ol start={page*PAGE_SIZE+1}>
-
-      {currentComments.map((comment)=>{
-        return <li key={comment.id}>{comment.name}</li>
-      })}
-      </ol>
-
-<button disabled={page===0} onClick={handlePrevClick}>prev</button>
-<button disabled={startIndex+PAGE_SIZE>=allComments.length} onClick={handleNextClick}>next</button>
-      
+    <div>
+      <input ref={inputFileRef} type='file' hidden/>
+      <button onClick={handleClick}>Browse files</button>
     </div>
   )
 }
 
 export default Demo
+
